b77bbb333d59398268daf0baff76fa974ee835f0
yehor <e.khodysko@gmail.com>
2024-03-25 11:46:40 +0200

    git and split remaps added

--- lua/lemonJuice/lazy/git.lua
+++ lua/lemonJuice/lazy/git.lua
@@ -3,7 +3,9 @@ return {
         'tpope/vim-fugitive',
 
         config = function()
-            vim.keymap.set('n', '<leader>G', vim.cmd.Git)
+            vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
+            vim.keymap.set('n', '<leader>gc', ':Git commit -m "')
+            vim.keymap.set('n', '<leader>gp', function() vim.cmd('Git push') end)
         end
     },
     {
--- lua/lemonJuice/remaps.lua
+++ lua/lemonJuice/remaps.lua
@@ -1,3 +1,18 @@
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>l', vim.cmd.Ex)
+
+-- Splits
+vim.keymap.set('n', '<leader>sX', function() vim.cmd('vertical split') end)
+vim.keymap.set('n', '<leader>sY', function() vim.cmd('split') end)
+
+
+vim.keymap.set('n', '<leader>sh', '<C-W>h')
+vim.keymap.set('n', '<leader>sj', '<C-W>j')
+vim.keymap.set('n', '<leader>sk', '<C-W>k')
+vim.keymap.set('n', '<leader>sl', '<C-W>l')
+
+vim.keymap.set('n', '<leader>sx+', function() vim.cmd('vertical resize +5') end)
+vim.keymap.set('n', '<leader>sx-', function() vim.cmd('vertical resize -5') end)
+vim.keymap.set('n', '<leader>sy+', function() vim.cmd('resize +5') end)
+vim.keymap.set('n', '<leader>sy-', function() vim.cmd('resize -5') end)
